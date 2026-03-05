c10a9227646a41290e8a5d60c9d0960d5e235028
GrieezBtc <muhammadjamiuyusuf876@gmail.com>
2026-03-05 01:35:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-const CACHE_NAME = 'grieezboy-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
-];
-
-// Install: Save the app files to the phone
+// sw.js
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  console.log('[GrieezBoy] Service Worker Installed');
 });
 
-// Fetch: Try to get from internet, but use cache if offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
